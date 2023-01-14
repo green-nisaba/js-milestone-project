e50c1f9d07d98febf1b746a9c5129a6e5463fa15
green-nisaba <basilandsalt@gmail.com>
2023-01-14 18:18:00 +0000

    Add questions to quiz

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+let questionsRange = [
+    {
+      questionImage:
+        "https://upload.wikimedia.org/wikipedia/commons/d/da/Flag_of_Luxembourg.svg",
+      answer1: "Luxembourg",
+      answer2: "Greece",
+      answer3: "Italy",
+      answer4: "Latvia",
+      correctA: 1
+    },
+    {
+      questionImage:
+        "https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg",
+      answer1: "Spain",
+      answer2: "India",
+      answer3: "Monaco",
+      answer4: "Niger",
+      correctA: 2
+    },
+    {
+      questionImage:
+        "https://upload.wikimedia.org/wikipedia/commons/2/20/Flag_of_the_Netherlands.svg",
+      answer1: "Luxembourg",
+      answer2: "Ireland",
+      answer3: "Netherlands",
+      answer4: "Hungury",
+      correctA: 3
+    },
+    {
+      questionImage:
+        "https://upload.wikimedia.org/wikipedia/commons/4/48/Flag_of_Singapore.svg",
+      answer1: "Singapore",
+      answer2: "Turkey",
+      answer3: "Tunisia",
+      answer4: "Pakistan",
+      correctA: 1
+    },
+    {
+      questionImage:
+        "https://upload.wikimedia.org/wikipedia/commons/f/fe/Flag_of_Uruguay.svg",
+      answers1: "Uruguay",
+      answer2: "Paraguay",
+      answer3: "Guatemala",
+      answer4: "Fiji",
+      correctA: 1
+    }
+  ];
