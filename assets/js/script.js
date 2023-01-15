2f89b3938a98316402d22eb4dd68fab2629eb194
green-nisaba <basilandsalt@gmail.com>
2023-01-15 15:21:24 +0000

    Add shoQuiz function to show questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,3 +45,26 @@ let questionsRange = [
       correctA: 1
     }
   ];
+
+  let ongoingQuiz = 0;
+
+const opt1 = document.getElementById("option1");
+const opt2 = document.getElementById("option2");
+const opt3 = document.getElementById("option3");
+const opt4 = document.getElementById("option4");
+
+
+
+function showQuiz() {
+  let quest = questionsRange[ongoingQuiz];
+  document.getElementById("question-image").innerHTML =
+    "<img src=" + quest.questionImage + ">";
+  opt1.innerHTML = quest.answer1;
+  opt2.innerHTML = quest.answer2;
+  opt3.innerHTML = quest.answer3;
+  opt4.innerHTML = quest.answer4;
+}
+
+
+
+showQuiz();
