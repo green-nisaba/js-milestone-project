42ec88d1efa997d4a1a70be8453858d40ad679d9
green-nisaba <basilandsalt@gmail.com>
2023-01-21 18:01:00 +0000

    Enable saving scores in local storage

---
+++ assets/js/finish.js
@@ -0,0 +1,33 @@
+const attemptName = document.getElementById("username");
+const saveScoreButton = document.getElementById("saveScoreB");
+const quizResult = document.getElementById("finalScore"); 
+const latestScore = localStorage.getItem("recentScore");
+quizResult.innerText = `${latestScore}/23`; 
+
+
+const finalResults = JSON.parse(localStorage.getItem('finalResults')) || [];
+const limitFinalResults = 3; 
+
+
+attemptName.addEventListener('keyup', ()  => {
+    saveScoreButton.disabled = !attemptName.value
+});
+function saveScore(event){
+    event.preventDefault();
+
+
+
+    const score = {
+        score: latestScore, 
+        name: attemptName.value
+    };
+    finalResults.push(score);
+    finalResults.sort((a, b) => b.score - a.score);
+    finalResults.splice(4);
+
+    localStorage.setItem('finalResults', JSON.stringify(finalResults));
+    window.location.assign('/');
+    console.log(finalResults);
+
+}
+
